1f6a1778059696a01f5dad6967e77ca9c182511d
Kira-god <kirayuzo189@gmail.com>
2024-03-27 15:33:47 +0630

    first commit

---
+++ Day24(VueJSSelfStudy1)/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="toggleMessage">Click Me</button>
+    <p v-if="showMessage">Hello World</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      showMessage: false,
+    };
+  },
+  methods: {
+    toggleMessage() {
+      this.showMessage = !this.showMessage;
+    },
+  },
+};
+</script>
+<style></style>
