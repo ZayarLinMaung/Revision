ddf84f237dd0d3b0e948deb4354c911274e34f5d
Kira-god <kirayuzo189@gmail.com>
2024-04-02 20:35:54 +0630

    first commit

--- Day26(VueJSRevision1)/src/App.vue
+++ Day26(VueJSRevision1)/src/App.vue
@@ -1,47 +1,59 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <section>
+    <div v-show="display" class="modal">
+      <h1>{{ greet }} {{ name }}</h1>
+      <div v-text="channel"></div>
+      <div v-html="element"></div>
+      <div v-html="link"></div>
+      <h2 v-if="val >= 5">val is greater than or equal 5</h2>
+      <h2 v-else="val < 5">val is less than 5</h2>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </section>
+  <section>
+    <button @click="showDisplay">Toggle</button>
+  </section>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+export default {
+  data() {
+    return {
+      greet: "Hello",
+      name: "ZLM",
+      channel: "Genesis",
+      element: "<b><p>Hello World</p></b>",
+      link: "<a href='#'>This is a link</a>",
+      val: 4,
+      display: false,
+    };
+  },
+  methods: {
+    showDisplay() {
+      this.display = !this.display;
+    },
+  },
+};
+</script>
+
+<style>
+section {
+  display: grid;
+  justify-content: center;
+  margin-bottom: 30px;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.modal {
+  width: 400px;
+  box-shadow: 0px 0px 10px black;
+  display: grid;
+  justify-content: center;
+  text-align: center;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+button {
+  padding: 10px;
+  width: 100px;
+  border-radius: 30px;
+  border: 1px solid black;
+  background-color: black;
+  color: white;
 }
 </style>
